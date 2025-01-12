ce240fd02ab2707505c0b2b248910af9dbf15a27
brenohsilva <brennohsilva@hotmail.com>
2025-01-12 13:19:08 -0300

    :sparkles: create hook to register point

---
+++ src/hooks/useRegisterPoint.ts
@@ -0,0 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import { recordsService } from "../services/recordsService";
+
+export function useRegisterPoint() {
+  const navigate = useNavigate();
+
+  const registerPoint = async () => {
+    try {
+      const response = await recordsService.setRecord();
+      alert("Ponto registrado com sucesso!");
+      navigate("/home");
+    } catch (err: any) {
+      console.error("Erro ao registrar ponto:", err);
+      alert("Erro ao registrar ponto. Tente novamente.");
+    }
+  };
+
+  return { registerPoint };
+}
