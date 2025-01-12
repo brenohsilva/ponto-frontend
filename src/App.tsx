a502d4e6a82e90e704f3ecb2c8dd4f9801b80482
brenohsilva <brennohsilva@hotmail.com>
2025-01-11 22:46:31 -0300

    :tada: first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Home from "./pages/home/home";
+import Login from "./pages/login";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
